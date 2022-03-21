6178cf4cb5c0c983a586e08d2906573acbdda3e8
Mohammad <s_m.reyhani@hotmail.com>
2022-03-21 11:26:29 +0330

    Play sound

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const note = document.querySelector('.nowPlaying');
 const keys = document.querySelectorAll('.key');
+const hints = document.querySelectorAll('.hints');
 
 window.addEventListener('keydown', function (e) {
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -7,10 +8,18 @@ window.addEventListener('keydown', function (e) {
 	const keyNote = key.getAttribute('data-note');
 	key.classList.add('playing');
 	note.innerHTML = keyNote;
+
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	audio.currentTime = 0;
+	audio.play();
 });
 
 function removeTransition() {
 	this.classList.remove('playing');
 }
+
+hints.forEach(function (elm, index) {
+	elm.setAttribute('style', `transition-delay: ${index * 50}ms`);
+});
 
 keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
