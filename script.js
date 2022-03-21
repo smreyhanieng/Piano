e0bacf76f8d929c4b34bd607b50ef7e6d2dd18a2
Mohammad <s_m.reyhani@hotmail.com>
2022-03-21 11:21:34 +0330

    Set key down animation

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const note = document.querySelector('.nowPlaying');
+const keys = document.querySelectorAll('.key');
 
 window.addEventListener('keydown', function (e) {
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 	if (!key) return;
 	const keyNote = key.getAttribute('data-note');
+	key.classList.add('playing');
 	note.innerHTML = keyNote;
 });
+
+function removeTransition() {
+	this.classList.remove('playing');
+}
+
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
